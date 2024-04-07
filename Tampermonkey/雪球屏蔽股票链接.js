ab79d992f8c24621b52e0a9f234cc30b7efef8af
zungmou <zungmou@gmail.com>
2024-04-07 11:13:25 +0800

    Add 雪球页面格式化的油猴脚本

---
+++ Tampermonkey/雪球屏蔽股票链接.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         雪球屏蔽股票链接
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  标点符号格式化，确保复制的一致性。
+// @author       Dylan
+// @match        https://xueqiu.com/*/*
+// @icon         https://www.google.com/s2/favicons?domain=xueqiu.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const location = window.location.href
+
+    if (/https:\/\/xueqiu.com\/\d+\/\d+/.test(location)) {
+        for (const anchor of document.querySelectorAll('a')) {
+            if (anchor.classList.contains('xq_stock')) {
+                anchor.replaceWith(anchor.textContent)
+            }
+        }
+    }
+})();
---
+++ Tampermonkey/雪球标点符号复位.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         雪球标点符号复位
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  标点符号格式化，确保复制的一致性。
+// @author       Dylan
+// @match        https://xueqiu.com/*/*
+// @icon         https://www.google.com/s2/favicons?domain=xueqiu.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    const location = window.location.href
+
+    if (/https:\/\/xueqiu.com\/\d+\/\d+/.test(location)) {
+        for (const hchar of document.querySelectorAll('h-char')) {
+            hchar.replaceWith(hchar.textContent)
+        }
+    }
+})();
